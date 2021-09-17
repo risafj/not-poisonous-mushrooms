5c4f7bec228ab20464b0d367e4c1a3a584c77140
Risa Fujii <risaf1231@gmail.com>
2021-09-17 22:25:28 +0900

    feat: Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import type { AppProps } from 'next/app'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#b8e951',
+      main: '#85b717',
+      dark: '#538700',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#9a5742',
+      main: '#682c1b',
+      dark: '#3b0200',
+      contrastText: '#fff',
+    },
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 export default MyApp
