d8cef740bd86975be33f18b245dfdd812deaacba
Risa Fujii <risaf1231@gmail.com>
2021-09-17 22:04:32 +0900

    refactor: Remove style files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Button, TextField } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
 import 'tailwindcss/tailwind.css'
 
 const Home: NextPage = () => {
