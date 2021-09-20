77a0071f6ec6de57f5d6150b5ff20ff48a81f96b
Robert Sherling <5183016+Rob117@users.noreply.github.com>
2021-09-20 17:14:45 +0900

    Add routing to header

--- components/compounds/Header.tsx
+++ components/compounds/Header.tsx
@@ -1,26 +1,35 @@
 import { Button, Divider, Typography } from '@mui/material';
 import React from 'react';
+import { useRouter } from 'next/router';
 import { DrawerMenuItem } from '../molecules/DrawerMenuItem';
 import { DrawerMenu } from './DrawerMenu';
 import { AddBoxOutlined, ExitToAppOutlined, LocalOfferOutlined, MailOutlined } from '@mui/icons-material';
 import { LanguagePicker } from '../molecules/LanguagePicker';
 import { SupportedLocale } from '../../@types';
+import { pathFromComponents } from '../../utils/linking';
 
 type MenuProps = {
   locale: SupportedLocale
 };
 const Menu = React.memo<MenuProps>(({ locale }) => {
+  const router = useRouter();
+
+  // consider extracting to generic routing function
+  const handleButtonPress = (path: string) => {
+    router.push(pathFromComponents(locale, path));
+  };
+
   return (
     <>
       <div className="hidden md:flex flex-row justify-between w-128">
         <LanguagePicker locale={ locale }/>
-        <Button variant="text" color="secondary">
+        <Button variant="text" color="secondary" onClick={ () => handleButtonPress('/login') }>
           Pricing
         </Button>
-        <Button variant="text" color="secondary">
+        <Button variant="text" color="secondary" onClick={ () => handleButtonPress('/login') }>
           Contact us
         </Button>
-        <Button variant="contained" color="secondary">
+        <Button variant="contained" color="secondary" onClick={ () => handleButtonPress('/login') }>
           Login
         </Button>
       </div>
@@ -28,11 +37,11 @@ const Menu = React.memo<MenuProps>(({ locale }) => {
         <LanguagePicker locale={ locale }/>
       </div>
       <DrawerMenu className="md:hidden">
-        <DrawerMenuItem icon={ <LocalOfferOutlined /> } label="Pricing" />
-        <DrawerMenuItem icon={ <MailOutlined /> } label="Contact us" />
+        <DrawerMenuItem icon={ <LocalOfferOutlined /> } label="Pricing" locale={ locale } path='/'/>
+        <DrawerMenuItem icon={ <MailOutlined /> } label="Contact us" locale={ locale } path='/'/>
         <Divider variant="middle" />
-        <DrawerMenuItem icon={ <AddBoxOutlined /> } label="Sign up" />
-        <DrawerMenuItem icon={ <ExitToAppOutlined /> } label="Log in" />
+        <DrawerMenuItem icon={ <AddBoxOutlined /> } label="Sign up" locale={ locale } path='/'/>
+        <DrawerMenuItem icon={ <ExitToAppOutlined /> } label="Log in" locale={ locale } path='/login'/>
       </DrawerMenu>
     </>
   );
