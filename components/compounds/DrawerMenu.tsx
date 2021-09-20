5f8f33d92883f7d27de733aaa5545fa779c889e2
Risa Fujii <risaf1231@gmail.com>
2021-09-20 12:54:42 +0900

    refactor: Reorder props and destructure them

--- components/compounds/DrawerMenu.tsx
+++ components/compounds/DrawerMenu.tsx
@@ -3,17 +3,17 @@ import { Drawer, IconButton } from '@mui/material';
 import React, { useCallback, useState } from 'react';
 
 type Props = {
-  className?: string
   children: React.ReactNode
-}
+  className?: string
+};
 
-export const DrawerMenu = (props: Props) => {
+export const DrawerMenu = ({ children, className }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDrawer = useCallback(() => setIsOpen(!isOpen), [isOpen]);
 
   return (
-    <div className={ props.className }>
+    <div className={ className }>
       <IconButton onClick={ toggleDrawer }>
         <Menu fontSize="large" />
       </IconButton>
@@ -24,7 +24,7 @@ export const DrawerMenu = (props: Props) => {
         ModalProps={ { onBackdropClick: toggleDrawer } }
       >
         <div className="w-56 mt-6">
-          { props.children }
+          { children }
         </div>
       </Drawer>
     </div>
