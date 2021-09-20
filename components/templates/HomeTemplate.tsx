504839aa67ffc88426192f66c7ce0a104b3cc1d6
Risa Fujii <risaf1231@gmail.com>
2021-09-20 19:14:36 +0900

    refactor: Move feature sections into main return statement

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -9,54 +9,6 @@ import { Section } from '../atoms/Section';
 import { Header } from '../compounds/Header';
 import { FeatureSection } from '../molecules/FeatureSection';
 
-const FeatureSections = () => {
-  return (
-    <>
-      <Section backgroundColor="primary.background">
-        <FeatureSection
-          text="Organic, locally sourced, free-range mushrooms."
-          imageElement={
-            <Image
-              src={ assetUrls.happyMushroom }
-              alt="happy mushroom"
-              width={ 200 }
-              height={ 200 }
-            />
-          }
-        />
-      </Section>
-      <Section>
-        <FeatureSection
-          text="Our QA teams ensure that the mushrooms are not poisonous."
-          imageElement={
-            <iframe
-              width="560"
-              height="315"
-              src={ assetUrls.qaTeam }
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          }
-          isImageOnLeft
-        />
-      </Section>
-      <Section backgroundColor="primary.background">
-        <FeatureSection
-          text="Loreming the ipsums. There should be 3 sections."
-          imageElement={
-            <Image
-              src={ assetUrls.happyMushroom }
-              alt="dancing mushroom"
-              width={ 200 }
-              height={ 200 }
-            />
-          }
-        />
-      </Section>
-    </>
-  );};
-
 export const HomeTemplate: TopLevelTemplate = ({ locale }) => {
   return (
     <div className="h-screen min-h-screen px-2 flex flex-col items-center">
@@ -79,7 +31,48 @@ export const HomeTemplate: TopLevelTemplate = ({ locale }) => {
             <Button variant="contained" className="flex w-40 self-center md:self-auto">Start now</Button>
           </div>
         </Section>
-        <FeatureSections />
+        <Section backgroundColor="primary.background">
+          <FeatureSection
+            text="Organic, locally sourced, free-range mushrooms."
+            imageElement={
+              <Image
+                src={ assetUrls.happyMushroom }
+                alt="happy mushroom"
+                width={ 200 }
+                height={ 200 }
+              />
+            }
+          />
+        </Section>
+        <Section>
+          <FeatureSection
+            text="Our QA teams ensure that the mushrooms are not poisonous."
+            imageElement={
+              <iframe
+                width="560"
+                height="315"
+                src={ assetUrls.qaTeam }
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            }
+            isImageOnLeft
+          />
+        </Section>
+        <Section backgroundColor="primary.background">
+          <FeatureSection
+            text="Loreming the ipsums. There should be 3 sections."
+            imageElement={
+              <Image
+                src={ assetUrls.happyMushroom }
+                alt="dancing mushroom"
+                width={ 200 }
+                height={ 200 }
+              />
+            }
+          />
+        </Section>
       </main>
 
       <Section>
