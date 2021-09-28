4ef731ac05617403c198fd1ee13832583b2024b1
Robert Sherling <5183016+Rob117@users.noreply.github.com>
2021-09-28 12:31:34 +0900

    Allow classes to be attached to sections

--- components/atoms/Section.tsx
+++ components/atoms/Section.tsx
@@ -1,16 +1,17 @@
-import { Box } from '@mui/material';
+import { Box, BoxProps } from '@mui/material';
 import React from 'react';
 
 type Props = {
   children: React.ReactNode
   backgroundColor?: string
-};
+} & BoxProps;
 
-export const Section = ({ children, backgroundColor }: Props) => {
+export const Section = ({ children, backgroundColor, ...props }: Props) => {
   return (
     <Box
       className="w-screen"
       sx={ { backgroundColor } }
+      { ...props }
     >
       <div className="container mx-auto text-center px-4 py-8 md:px-8">
         { children }
