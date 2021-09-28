99e1adb3aa5401161b6d14f059ffda176b6931b1
Robert Sherling <5183016+Rob117@users.noreply.github.com>
2021-09-28 12:31:12 +0900

    Add animation keyframes according to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const fadeInAnimationKeyframes = {
+  '0%': { opacity: 0 }, '100%': { opacity: 1 } 
+};
+
+// https://codepen.io/jamesdoc/pen/qBbeOym
+const fadeInAnimationRules = 'fadeIn 1s ease-in forwards';
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: fadeInAnimationKeyframes
+      },
+      animation: {
+        fadeIn: fadeInAnimationRules
+      }
+    },
   },
   variants: {
     extend: {},
