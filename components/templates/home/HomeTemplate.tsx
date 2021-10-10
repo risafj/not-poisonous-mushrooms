5295ad33fe6b72590537400a8f1364eab057e6a3
Risa Fujii <risaf1231@gmail.com>
2021-10-10 16:31:13 +0900

    feat: OGP settings for HomeTemplate

--- components/templates/home/HomeTemplate.tsx
+++ components/templates/home/HomeTemplate.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import React, { useContext } from 'react';
 import 'tailwindcss/tailwind.css';
 import { TopLevelTemplate } from '../../../@types';
+import { assetUrls } from '../../../utils/assetUrls';
 import { Section } from '../../atoms/Section';
 import { TranslatedText } from '../../atoms/TranslatedText';
 import { TranslationContext } from '../../page_wrappers/TranslationsWrapper';
@@ -17,6 +18,15 @@ export const HomeTemplate: TopLevelTemplate = () => {
         <title>{ translation.homepageSEO.title }</title>
         <meta name="description" content={ translation.homepageSEO.description }/>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="npm.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Not Poisonous Mushrooms" />
+        <meta property="og:description" content="Premium mushroom subscription service" />
+        <meta property="og:image" content={ assetUrls.ogp } />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="315" />
+        <meta name="twitter:title" content="Not Poisonous Mushrooms" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <main className="flex flex-col justify-start">
